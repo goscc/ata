b3065a21b5097a58a4d71bd9d9a69887b777f9d7
lio19 <liquanqing110@qq.com>
2022-09-01 10:53:17 +0800

    [feat] add api /sub/get/offer

--- src/signal/mapper/bean.rs
+++ src/signal/mapper/bean.rs
@@ -1,8 +1,19 @@
 use serde::{Serialize, Deserialize};
 
 #[derive(Serialize, Deserialize)]
+pub struct ReqOffer {
+    pub sdp: String,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct ReqGetOffer{
+   pub id: u32,
+}
+
+
 pub struct Offer {
     pub sdp: String,
+    pub add_time: u64,
 }
 
 // pub struct Answer {
--- src/signal/mapper/mod.rs
+++ src/signal/mapper/mod.rs
@@ -3,16 +3,17 @@ mod bean;
 
 use std::collections::HashMap;
 use std::sync::Mutex;
+use std::time::{Duration, SystemTime, UNIX_EPOCH};
 use hyper::{Body, Request, Response};
 use serde_json::json;
-use crate::mapper::bean::Offer;
+use crate::mapper::bean::{Offer, ReqGetOffer, ReqOffer};
 use crate::Method;
 
 
 type R = Result<Response<Body>, hyper::Error>;
 
 pub struct Mapper {
-    pub id_to_offer: Mutex<HashMap<u32, String>>,
+    pub id_to_offer: Mutex<HashMap<u32, Offer>>,
     pub counter: Mutex<u32>
 }
 
@@ -27,6 +28,7 @@ impl Mapper{
     pub async fn handler(&self, req :Request<Body>) -> R  {
         match (req.method(), req.uri().path()) {
             (&Method::POST, "/pub/offer") => pub_offer(self, req).await,
+            (&Method::POST, "/sub/get/offer") => sub_get_offer(self, req).await,
             _ => Ok(Response::new(Body::from("not found"))),
         }
     }
@@ -38,20 +40,70 @@ impl Mapper{
     }
 }
 
+
+//sub/get/offer
+async fn sub_get_offer(m: &Mapper, req :Request<Body>) -> R {
+    let req_body = hyper::body::to_bytes(req.into_body()).await?;
+    let r = serde_json::from_slice(&req_body);
+
+    if r.is_err() {
+        return error_resp(1, "request body is not offer")
+    }
+
+    let rgo :ReqGetOffer = r.unwrap();
+
+    let mut ito = m.id_to_offer.lock().unwrap();
+    let op = ito.remove(&rgo.id);
+
+    if op.is_none() {
+        //todo remove
+        return error_resp(1, "do not exit this offer id")
+    }
+
+    let of : Offer = op.unwrap();
+
+
+    let resp_json = json!({
+        "stat": 0,
+        "msg": "success",
+        "data": {
+            "sdp": of.sdp,
+        }
+    });
+    //todo: remove
+
+    Ok(Response::new(Body::from(resp_json.to_string())))
+}
+
 //pub/offer
 async fn pub_offer(m: &Mapper, req :Request<Body>) -> R {
     let req_body = hyper::body::to_bytes(req.into_body()).await?;
 
-    let Err(e) = serde_json::from_slice( &req_body);
-    if Err(e) {
-        return error(1, "sdp is nil")
+    let r  = serde_json::from_slice( &req_body);
+    if r.is_err() {
+        return error_resp(1, "request body is not offer")
     }
-    let s :Offer = Ok(s).unwrap();
+
+    let ro :ReqOffer = r.unwrap();
+
 
     let id = m.create_id();
 
     let mut ito = m.id_to_offer.lock().unwrap();
-    ito.insert(id, s.sdp);
+
+    let r  = SystemTime::now().duration_since(UNIX_EPOCH);
+    if r.is_err() {
+        return error_resp(2, "server error, get time fail");
+    }
+
+    let cur_time: Duration = r.unwrap();
+
+    let offer = Offer{
+        sdp: ro.sdp,
+        add_time: cur_time.as_secs(),
+    };
+
+    ito.insert(id, offer);
 
     let resp_json = json!({
         "stat": 0,
@@ -64,7 +116,7 @@ async fn pub_offer(m: &Mapper, req :Request<Body>) -> R {
 }
 
 
-fn error(stat :u32, msg: &str) -> R {
+fn error_resp(stat :u32, msg: &str) -> R {
    let resp_json = json!({
        "stat": stat,
        "msg": msg,
@@ -72,6 +124,3 @@ fn error(stat :u32, msg: &str) -> R {
 
    Ok(Response::new(Body::from(resp_json.to_string())))
 }
-
-
-//p

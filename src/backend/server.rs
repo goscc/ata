afd114e7aca17fe67a91c3c4a45a1b7474910a20
binarytom <lieh_666@foxmail.com>
2022-09-03 20:24:12 +0800

    mv server to pub.

--- src/backend/server.rs
+++
@@ -1,7 +0,0 @@
-use actix_web::{guard, web, App, HttpServer, Responder, HttpResponse, get};
-use actix_files::{NamedFile};
-
-#[get("/")]
-async fn index() -> std::io::Result<NamedFile> {
-    Ok(NamedFile::open(std::path::PathBuf::from("./pages/client.html"))?)
-}

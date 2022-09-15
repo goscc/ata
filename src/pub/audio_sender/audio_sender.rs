13f5607ee728b70291efdca355c5831974b506d8
lio19 <liquanqing110@qq.com>
2022-09-15 23:51:20 +0800

    temp: add webrtc-rs

--- src/pub/audio_sender/audio_sender.rs
+++ src/pub/audio_sender/audio_sender.rs
@@ -1,16 +1,65 @@
+use webrtc::api::APIBuilder;
+use webrtc::api::interceptor_registry::register_default_interceptors;
+use webrtc::api::media_engine::{MediaEngine, MIME_TYPE_OPUS};
+use webrtc::ice_transport::ice_server::RTCIceServer;
+use webrtc::interceptor::registry::Registry;
+use webrtc::peer_connection::configuration::RTCConfiguration;
+use webrtc::rtp_transceiver::rtp_codec::{RTCRtpCodecCapability, RTCRtpCodecParameters};
+use webrtc::rtp_transceiver::rtp_codec::RTPCodecType::Audio;
+
 pub struct AudioSender {
 }
 
+//
 //1. begin
 //2. 开始
-//
 impl AudioSender {
     pub fn new() -> AudioSender {
         AudioSender{}
     }
 
+    //init
+    //1. create engine
     pub fn init() {
+
+
+
+    }
+
+    //1. create engine
+    //2. create offer
+    //3. send offer
+    //4. get answer
+    //5. send opus
+    pub fn run()  {
         let mut m = MediaEngine::default();
+        m.register_codec(RTCRtpCodecParameters {
+            capability: RTCRtpCodecCapability {
+                mime_type: MIME_TYPE_OPUS.to_owned(),
+                clock_rate: 48000,
+                channels: 2,
+                sdp_fmtp_line: "minptime=10;useinbandfec=1".to_owned(),
+                rtcp_feedback: vec![],
+            },
+            payload_type: 111,
+            ..Default::default()
+        }, Audio)?;
+
+        let mut registry = Registry::new();
+        registry = register_default_interceptors(registry, &mut m)?;
+        let api = APIBuilder::new()
+            .with_media_engine(m)
+            .with_interceptor_registry(registry)
+            .build();
+
+        // Prepare the configuration
+        let config = RTCConfiguration {
+            ice_servers: vec![RTCIceServer {
+                urls: vec!["stun:stun.l.google.com:19302".to_owned()],
+                ..Default::default()
+            }],
+            ..Default::default()
+        };
     }
 
     pub fn end() {
